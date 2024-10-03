f0dd84a36d2845c1a5b97d10f3d7787390f44ad7
Lê Đình Mạnh <manhld@rabiloo.com>
2024-10-03 13:28:42 +0700

    Add show process_id to logger

--- src/fastapi_base/logger/formatter/__init__.py
+++ src/fastapi_base/logger/formatter/__init__.py
@@ -1,5 +1,6 @@
 DEFAULT_FORMATTER = (
     "<level>{level: <8}</level> <green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> "
+    "- <green>{process}</green> "
     "- <cyan>{name}</cyan>:<cyan>{function}</cyan> "
     "- <level>{message}</level>"
 )
