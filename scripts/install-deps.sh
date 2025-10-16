b6e675fe299e7bc7cb869256f106fcf856034a1b
duydq <duydq@rabiloo.com>
2025-10-16 16:30:17 +0700

    chore: update GitHub Actions workflows and dependencies for improved CI/CD and package management

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -7,7 +7,7 @@ pip install ruff typos
 # Type
 pip install mypy
 # Test
-pip install pytest coverage pytest-cov pytest-asyncio
+pip install pytest coverage pytest-cov pytest-asyncio httpx numpy
 
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
 if [ -f requirements-dev.txt ]; then pip install -r requirements-dev.txt; fi
